35508af922cd74e7e231acc75d2e178ce2555c56
kowaraj <kowaraj4stuff@gmail.com>
2020-10-07 20:17:53 +0200

    Get a field of a RE-record from JS world

--- src/Db.re
+++ src/Db.re
@@ -1,7 +1,23 @@
+// ReasonML wrapper for the Firestore database interface in Firestore.js
 
 [@bs.module "./Firestore.js"]
 external fs_memes_onSnapshot: ( (string, TT.fs_meme_t) => unit) => unit = "memes_onSnapshot";
 
+[@bs.val] external document: 'a = "document";
+// call a method
+document##addEventListener("mouseup", _event => {
+  let loc = document##location;
+
+  ////    document##location##href = "rescript-lang.org";
+
+  Js.log("clicked!");
+});
+
+// get a property
+
+// set a property
+
+[@bs.get] external getName: TT.fs_meme_t => string = "text";
 
 [@react.component]
 let make = () => {
@@ -9,11 +25,16 @@ let make = () => {
     let (memes, setMemes) = React.useState( () => [] );
 
     let add_meme = (m_id, m:TT.fs_meme_t) => {
-        Js.log(">>> add_meme:")
-        Js.log(m)
-//        Js.log(nameGet(m))
+        Js.log("Db.re: add_meme:")
+//        Js.log(m)
+        Js.log(getName(m))
+        Js.log(m.name)
+
+
+
 
         let mm : TT.fs_meme_t = {id: m_id, name: m.name, text: m.text, fn: m.fn, tags: m.tags};
+//        Js.log(mm)
         setMemes( ms => List.append(ms, [ mm ] ) )
     };
 
--- src/Firestore.js
+++ src/Firestore.js
@@ -1,3 +1,6 @@
+// Interface to the Firestore database
+// Used by a wrapper, see Db.re
+
 //real-time listener
 //firebase send a snapshot of the DB when it changed
 
@@ -6,12 +9,27 @@ var memes_onSnapshot = (
 function(cb) {
     firestore_db.collection('memes').onSnapshot( snapshot => {
         //console.log(snapshot.docChanges())
-
         snapshot.docChanges().forEach(change => {
             if (change.type === 'added') {
-                console.log(change.type, change.doc.data(), change.doc.id)
-//                cb(change.doc.data().id, change.doc.data().name, {name: change.doc.data().name, text: change.doc.data().text}, change.doc.data())
-                cb(change.doc.id, change.doc.data())
+                //console.log(change.type, change.doc.data(), change.doc.id)
+
+                var m = change.doc.data();
+//                console.log(m)
+                // var mm_001 = /* name */m[/* name */1];
+                // var mm_002 = /* text */m[/* text */2];
+                // var mm_003 = /* fn */m[/* fn */3];
+                // var mm_004 = /* tags */m[/* tags */4];
+                // var mm = /* record */[
+                //   /* id */m_id,
+                //   mm_001,
+                //   mm_002,
+                //   mm_003,
+                //   mm_004
+                // ];
+            
+                //                cb(change.doc.data().id, change.doc.data().name, {name: change.doc.data().name, text: change.doc.data().text}, change.doc.data())
+                cb(change.doc.id, m )
+
             }
             if (change.type === 'removed') {
                 // console.log(change)
